5c5aa800fe0b715e6cbab40cf010320f8dd68866
Cristian <cristian.christensen@gmail.com>
2023-03-07 12:06:42 +0100

    Added subpages for AccountPage

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,9 +1,15 @@
 import React, { useContext } from "react";
 import { UserContext } from "../UserContext";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate, useParams } from "react-router-dom";
 
 const AccountPage = () => {
   const { user, ready } = useContext(UserContext);
+
+  let { subpage } = useParams();
+
+  if (subpage === undefined) {
+    subpage = "profile";
+  }
 
   if (!ready) return <div>Loading...</div>;
 
@@ -11,7 +17,29 @@ const AccountPage = () => {
     return <Navigate to={"/login"} />;
   }
 
-  return <div>Account Page for {user?.name}</div>;
+  function linkClasses(path = null) {
+    let classes = "py-2 px-6";
+    if (subpage === path) {
+      classes += " bg-primary text-white rounded-full";
+    }
+    return classes;
+  }
+
+  return (
+    <div>
+      <nav className="w-full flex justify-center mt-8 gap-5">
+        <Link className={linkClasses("profile")} to={"/account/"}>
+          Profile
+        </Link>
+        <Link className={linkClasses("bookings")} to={"/account/bookings"}>
+          Bookings
+        </Link>
+        <Link className={linkClasses("properties")} to={"/account/properties"}>
+          Properties
+        </Link>
+      </nav>
+    </div>
+  );
 };
 
 export default AccountPage;
