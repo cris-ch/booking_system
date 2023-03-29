e55d67d9f486c3ec6ab5ffbc25c777aaee6c6472
Cristian <cristian.christensen@gmail.com>
2023-03-29 14:36:53 +0200

    * client/src/pages/PropertyPage.jsx: Add photos to property page.

--- client/src/pages/PropertyPage.jsx
+++ client/src/pages/PropertyPage.jsx
@@ -19,8 +19,47 @@ const PropertyPage = () => {
 
   return (
     <div className="mt-4 bg-gray-100 -mx-8 px-8 py-6">
-      <h1 className="text-xl">{property.title}</h1>
-      <a target="_blank" href={"https://maps.google.com/?q="+property.address}>{property.address}</a>
+      <h1 className="text-2xl">{property.title}</h1>
+      <a
+        className="my-1 block font-semibold underline"
+        target="_blank"
+        href={"https://maps.google.com/?q=" + property.address}
+      >
+        {property.address}
+      </a>
+      <div className="grid gap-2 grid-cols-[2fr_1fr]">
+        <div className="">
+          {property.photos?.[0] && (
+            <div className="">
+              <img
+                className="aspect-square object-cover"
+                src={"http://localhost:4000/uploads/" + property.photos[0]}
+                alt=""
+              />
+            </div>
+          )}
+        </div>
+        <div className="grid ">
+          {property.photos?.[1] && (
+            <img
+              className="aspect-square object-cover"
+              src={"http://localhost:4000/uploads/" + property.photos[1]}
+              alt=""
+            />
+          )}
+          <div className="overflow-hidden">
+            {property.photos?.[2] && (
+              <div className="">
+                <img
+                  className="aspect-square object-cover relative top-2"
+                  src={"http://localhost:4000/uploads/" + property.photos[2]}
+                  alt=""
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
