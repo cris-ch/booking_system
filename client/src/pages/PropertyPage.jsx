a40526194075d2e8ba392656b18487844820e0e4
Cristian <cristian.christensen@gmail.com>
2023-03-29 14:09:36 +0200

    * client/src/pages/PropertyPage.jsx: [PropertyPage]: Display address in Property Page with link

--- client/src/pages/PropertyPage.jsx
+++ client/src/pages/PropertyPage.jsx
@@ -14,11 +14,15 @@ const PropertyPage = () => {
       console.log(res.data);
     });
   }, [id]);
-  return <div className="mt-8 ">
-    {property && (
-      <h1>{property.title}</h1>
-    )}
-  </div>;
+
+  if (!property) return null;
+
+  return (
+    <div className="mt-4 bg-gray-100 -mx-8 px-8 py-6">
+      <h1 className="text-xl">{property.title}</h1>
+      <a target="_blank" href={"https://maps.google.com/?q="+property.address}>{property.address}</a>
+    </div>
+  );
 };
 
 export default PropertyPage;
