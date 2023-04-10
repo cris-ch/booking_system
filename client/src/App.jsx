91788ced220cc7cb871a0c31a21ec493393c874c
Cristian <cristian.christensen@gmail.com>
2023-04-10 17:33:53 +0200

    Base URL imported from environment variable to allow for different environments.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import PropertiesFormPage from "./pages/PropertiesFormPage";
 import PropertyPage from "./pages/PropertyPage";
 
 
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
