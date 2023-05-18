b89467b82ce621e12ed933f8fca102145243186a
Cristian <cristian.christensen@gmail.com>
2023-05-18 11:13:34 +0200

    Changed Search Bar Styling

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -5,10 +5,8 @@ const SearchBar = () => {
   return (
     <div className="flex gap-2 border border-color-gray-300 rounded-full py-2 px-4 shadow-md shadow-color-gray-300 items-center">
       <div>Anywhere</div>
-      <div className="border-l border-gray-300"></div>
-      <div>Anytime</div>
-      <div className="border-l border-gray-300"></div>
-      <div>Add guests</div>
+      <div className="border-l border-gray-300 pl-2">Anytime</div>
+      <div className="border-l border-gray-300 pl-2">Add guests</div>
       <button className="bg-primary text-white p-1 rounded-full">
         <BsSearch />
       </button>
